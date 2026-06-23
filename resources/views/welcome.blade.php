f17162408f9aac65ef3750e5292f4e10c1e2d0ad
JUSTIN <justinsunilsunil@gmail.com>
2026-06-23 18:18:00 +0530

    completed Laravel task

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel Assignment</title>
+
+    <style>
+        body{
+            margin:0;
+            padding:0;
+            height:100vh;
+            display:flex;
+            justify-content:center;
+            align-items:center;
+            background-color:#f4f4f4;
+            font-family:Arial, sans-serif;
+        }
+
+        .card{
+            background:white;
+            padding:40px;
+            border-radius:15px;
+            box-shadow:0 4px 15px rgba(0,0,0,0.2);
+            text-align:center;
+            width:450px;
+        }
+
+        h1{
+            color:#e63946;
+            margin-bottom:20px;
+        }
+
+        h2{
+            color:#333;
+            margin:10px 0;
+        }
+
+        p{
+            color:#666;
+            margin-top:20px;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div class="card">
+        <h1>Laravel Project</h1>
+
+        <h2>Name: Justin Sunil</h2>
+
+        <h2>Roll Number: 5024126</h2>
+
+        <p> Laravel Homepage using Laravel Herd (modified)</p>
+    </div>
+
+</body>
+</html>
